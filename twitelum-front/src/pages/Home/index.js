d409e8f7ed10deb08721d15fd2408876eb518eb1
lilolima <stefanielima_araujo@hotmail.com>
2018-04-13 20:07:58 -0300

    Integrar com a API

--- twitelum-front/src/pages/Home/index.js
+++ twitelum-front/src/pages/Home/index.js
@@ -6,82 +6,108 @@ import Widget from '../../components/Widget'
 import TrendsArea from '../../components/TrendsArea'
 import Tweet from '../../components/Tweet'
 
+
+
 class Home extends Component {
-    constructor() {
-        super()
-        this.state = {
-            novoTweet: '',
-            tweets: []
-        }
-        this.adicionaTweet = this.adicionaTweet.bind(this)
-    }
+  constructor(props) {
+    super()
 
-    adicionaTweet(infosDoEvento) {
-        infosDoEvento.preventDefault()
-        //Pegar o vaue do input
-        const novoTweet = this.state.novoTweet
-        const tweetsAntigos = this.state.tweets
-        if (novoTweet) {
-            this.setState({
-                tweets: [novoTweet, ...tweetsAntigos],
-                novoTweet: ''
-            })
-        }
-    }
+    this.state = {
+      novoTweet: '',
+      tweets: []
+    } 
+    // Código triste da vida
+    this.adicionaTweet = this.adicionaTweet.bind(this)
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <Cabecalho>
-                    <NavMenu usuario="@omariosouto" />
-                </Cabecalho>
-                <div className="container">
-                    <Dashboard>
-                        <Widget>
-                            <form className="novoTweet" onSubmit={this.adicionaTweet}>
-                                <div className="novoTweet__editorArea">
-                                    <span className={`
-                                        novoTweet__status
-                                        ${this.state.novoTweet.length > 140
-                                            ? 'novoTweet__status--invalido' : ''}
-                                        `}>
-                                        {this.state.novoTweet.length}/140
-                                    </span>
-                                    <textarea
-                                        className="novoTweet__editor"
-                                        value={this.state.novoTweet}
-                                        onInput={(event) => this.setState({ novoTweet: event.target.value })}
-                                        placeholder="O que está acontecendo?">
-                                    </textarea>
-                                </div>
-                                <button type="submit" className="novoTweet__envia"
-                                    disabled={this.state.novoTweet.length > 140 ? true : false}
-                                    type="submit">
-                                    Tweetar
-                            </button>
-                            </form>
-                        </Widget>
-                        <Widget>
-                            <TrendsArea />
-                        </Widget>
-                    </Dashboard>
-                    <Dashboard posicao="centro">
-                        <Widget>
-                            <div className="tweetsArea">
-                                {this.state.tweets.map(
-                                    (tweetInfo, index) =>
-                                        <Tweet
-                                            key={tweetInfo + index}
-                                            texto={tweetInfo} />
-                                )
+  componentDidMount() {
+    console.log('DidMount')
+    fetch(`http://localhost:3001/tweets?X-AUTH-TOKEN=${localStorage.getItem('TOKEN')}`)
+      .then((respostaDoServer) => respostaDoServer.json())
+      .then((tweetsDoServidor) => {
+        console.log(tweetsDoServidor)
+        this.setState({
+          tweets: tweetsDoServidor
+        })    
+      })
+  }
+
+  // Talk: Anjana Vakil: Learning Functional Programming with JavaScript - JSUnconf 2016
+  adicionaTweet(infosDoEvento) {    
+    infosDoEvento.preventDefault()
+    // Pegar o value do input
+    const novoTweet = this.state.novoTweet
+
+    if(novoTweet) {
+      // Manda o texto e o TOKEN
+      fetch(`http://localhost:3001/tweets?X-AUTH-TOKEN=${localStorage.getItem('TOKEN')}`,
+        { method: 'POST' ,body: JSON.stringify({ conteudo: novoTweet })  })
+      .then((respostaDoServer) => {
+          return respostaDoServer.json()
+      })
+      .then((tweetProntoDoServer) => {
+          console.log(tweetProntoDoServer)
+          this.setState({
+            tweets: [tweetProntoDoServer, ...this.state.tweets]
+          })
+      })
+    }
+  }
+
+  render() {
+
+    return (
+      <Fragment>
+        <Cabecalho>
+            <NavMenu usuario="@omariosouto" />
+        </Cabecalho>
+        <div className="container">
+            <Dashboard>
+                <Widget>
+                <form className="novoTweet" onSubmit={ this.adicionaTweet }>
+                        <div className="novoTweet__editorArea">
+                            <span className={`novoTweet__status ${
+                              this.state.novoTweet.length > 140
+                              ? 'novoTweet__status--invalido' : ''
+                            }`}>
+                              {this.state.novoTweet.length}/140
+                            </span>
+                            <textarea
+                              value={this.state.novoTweet}
+                              onChange={ (event) => { this.setState({ novoTweet: event.target.value }) } }
+                              className="novoTweet__editor"
+                              placeholder="O que está acontecendo?"></textarea>
+                        </div>
+                        <button type="submit"
+                                disabled={
+                                  this.state.novoTweet.length > 140
+                                  ? true : false
                                 }
-                            </div>
-                        </Widget>
-                    </Dashboard>
-                </div>
-            </Fragment >
-        );
-    }
+                                className="novoTweet__envia">Tweetar</button>
+                    </form>
+                </Widget>
+                <Widget>
+                    <TrendsArea />
+                </Widget>
+            </Dashboard>
+            <Dashboard posicao="centro">
+                <Widget>
+                    <div className="tweetsArea">
+                        { this.state.tweets.length === 0
+                          ? <div>Mensagem avisando</div> : ''
+                        }
+                        {
+                          this.state.tweets.map((tweet, index) =>
+                            <Tweet key={tweet + index} tweetInfo={tweet} texto={tweet.conteudo} />
+                          )
+                        }
+                    </div>
+                </Widget>
+            </Dashboard>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default Home;
--- twitelum-front/src/routes.js
+++ twitelum-front/src/routes.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import Home from './pages/Home'
+// Aqui fica tudo do roteamento
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Home from './pages/Home';
 import LoginPage from './pages/LoginPage'
 
-class Routes extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" component={LoginPage} />
-            </Switch>
-        )
+function estaAutenticado() {
+    if (localStorage.getItem('TOKEN')) {
+        return true
     }
-
+    return false
 }
 
+class PrivateRoute extends Component {
+    render() {
+        // const path = this.props.path
+        // const component = this.props.component
+        if (estaAutenticado()) {
+            return (
+                <Route {...this.props} />
+            )
+        } else {
+            return (
+                <Redirect to="/login" />
+            )
+        }
+    }
+}
+
+
+const Routes = () => {
+    return (
+        <Switch> {/*  Ele pega a URL e faz os ifs malditos */}
+            <PrivateRoute path="/" exact component={Home} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="*" component={() => (<div>Página 404</div>)} />
+        </Switch>
+    )
+}
 export default Routes
+
+// export default class Routes extends Component {
+//     render() {
+//         return (
+//             <Switch> {/*  Ele pega a URL e faz os ifs malditos */}
+//                 <Route path="/" exact component={Home}/>
+//                 <Route path="/login" component={LoginPage} />
+//                 <Route path="*" component={ () => (<div>Página 404</div>) } />
+//             </Switch>
+//         )
+//     }
+// }
